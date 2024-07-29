63db88dc3aa5bd4f4f831de4a9b4c88a287f6ab9
Hoi Cheng <49464204+catbree@users.noreply.github.com>
2024-07-29 19:42:12 +0800

    Added router and created routes for about, work, and homepage

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import HomePage from "./pages/HomePage";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/about", element: <div>Hello world, aboutpage!</div> },
+  { path: "/work", element: <div>Hello world, workpage!</div> },
+  { path: "/thoughts", element: <div>Hello world, thoughtpage, WIP!</div> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
