c842d4bf1edc72e1c35367f506ca7f9a466715bc
Hoi Cheng <49464204+catbree@users.noreply.github.com>
2024-07-31 13:35:58 +0800

    Configured ability to read and import markdown

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,21 @@
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import Markdown from "../entries/case-study-greenview.md";
+
+// export function HeroSection() {
+//   // eslint-disable-next-line
+//   return <ReactMarkdown children={Markdown} />;
+// }
+
 export function HeroSection() {
-    return <p>This is hero section.</p>
-  }
-  
+  const [text, setText] = useState(null);
+
+  fetch(Markdown)
+    .then((response) => response.text())
+    .then((importedText) => {
+      setText(importedText)
+    });
+
+  // eslint-disable-next-line
+  return <ReactMarkdown children={text} />;
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./styles/global.css";
+import "./fonts/Lexend-VariableFont_wght.ttf";
 
 import HomePage from "./pages/HomePage";
 import WorkPage from "./pages/WorkPage";
 import AboutPage from "./pages/AboutPage";
 import WorkEntryPage from "./pages/WorkEntryPage";
 
-const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/about", element: <AboutPage /> },
-  { path: "/work", element: <WorkPage /> },
-  { path: "work/:slug", element: <WorkEntryPage /> },
-]);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/work" element={<WorkPage />} />
+        <Route path="/work/:slug" element={<WorkEntryPage />} />
+        <Route path="*" element={<h1>Error Page</h1>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
