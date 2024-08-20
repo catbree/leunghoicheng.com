0bfbfe4023169f2bb2996aac4fdc428329a33a62
Hoi Cheng <49464204+catbree@users.noreply.github.com>
2024-08-20 15:57:21 +0800

    Added interactions to work page and color on hover

--- src/components/FeaturedWorkSection.jsx
+++ src/components/FeaturedWorkSection.jsx
@@ -11,12 +11,14 @@ export default function FeaturedWorkSection({ featuredWorkFrontMatter }) {
       className="featured-work-section"
       to={`/work${featuredWorkFrontMatter.slug}`}
     >
-      <div className="featured-work-image-container">
-        <img src={featuredWorkFrontMatter.imageUrl}></img>
-      </div>
-      <div className="featured-work-content-container">
-        <h2>{featuredWorkFrontMatter.title}</h2>
-        <p>Not sure where to start? I'd like to recommend this one.</p>
+      <div className="featured-work-container">
+        <div className="featured-work-image-container">
+          <img src={featuredWorkFrontMatter.imageUrl}></img>
+        </div>
+        <div className="featured-work-content-container">
+          <h2>{featuredWorkFrontMatter.title}</h2>
+          <p>Not sure where to start? I'd like to recommend this one.</p>
+        </div>
       </div>
     </Link>
   );
