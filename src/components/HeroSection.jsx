46baa95903f881e27107cf205cce2a1b61fbdc83
Hoi Cheng <49464204+catbree@users.noreply.github.com>
2024-08-20 17:11:25 +0800

    Turned sprite into gif to address jittering bug

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,12 @@ export default function HeroSection() {
   return (
     <div className="hero-section">
       <div className="sprite-container">
-        <div className="sprite"></div>
+        <div className="sprite">
+          <img
+            src="assets/hero-section-animation.gif"
+            alt="Animation featuring a large centered title that reads 'Hoi Cheng Product Designer,' with a black cat sitting on the letter G. The title is displayed above a pavement with grass. There are two clouds in the background."
+          ></img>
+        </div>
       </div>
     </div>
   );
