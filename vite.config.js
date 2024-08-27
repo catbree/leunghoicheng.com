d71e6494e29d5290860451c0a4f678f66a36f5ef
Hoi Cheng <49464204+catbree@users.noreply.github.com>
2024-08-27 23:05:16 +0800

    Added google analytics

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { VitePluginRadar } from 'vite-plugin-radar';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,11 @@ export default defineConfig({
         Buffer: true,
       },
     }),
+    VitePluginRadar({
+      analytics: {
+        id: 'G-FQYF7KHKNH',
+      }
+    })
   ],
   assetsInclude: ["**/*.md"],
 });
